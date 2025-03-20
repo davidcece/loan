c848eb38f21d3c9882a2c3a726f110de09d7f5c0
David Cece <dcece@Karatus.local>
2025-03-20 22:34:26 +0300

    Add logging to CBSIntegrationService

--- src/main/java/com/cece/lms/service/CoreBankingIntegrationService.java
+++ src/main/java/com/cece/lms/service/CoreBankingIntegrationService.java
@@ -1,6 +1,7 @@
 package com.cece.lms.service;
 
 import com.cece.lms.config.SOAPConnector;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
 import com.credable.soap.kyc.CustomerResponse;
@@ -9,6 +10,7 @@ import com.credable.soap.transactions.TransactionsRequest;
 import com.credable.soap.transactions.TransactionsResponse;
 
 @Service
+@Slf4j
 public class CoreBankingIntegrationService {
 
     private final SOAPConnector kycSOAPConnector;
@@ -18,23 +20,26 @@ public class CoreBankingIntegrationService {
                                          @Qualifier("transactionsSOAPConnector") SOAPConnector transactionsSOAPConnector) {
         this.kycSOAPConnector = kycSOAPConnector;
         this.transactionsSOAPConnector = transactionsSOAPConnector;
+        log.info("CoreBankingIntegrationService initialized with KYC and Transactions SOAP connectors");
     }
 
-
     public CustomerResponse getCustomerKYC(String customerNumber) {
+        log.info("Fetching KYC data for customer number: {}", customerNumber);
         CustomerRequest request = new CustomerRequest();
         request.setCustomerNumber(customerNumber);
 
-        return (CustomerResponse) kycSOAPConnector.callWebService(request);
+        CustomerResponse response = (CustomerResponse) kycSOAPConnector.callWebService(request);
+        log.info("Received KYC response for customer number: {}", customerNumber);
+        return response;
     }
 
-
     public TransactionsResponse getTransactionData(String customerNumber) {
+        log.info("Fetching transaction data for customer number: {}", customerNumber);
         TransactionsRequest request = new TransactionsRequest();
         request.setCustomerNumber(customerNumber);
 
-        return (TransactionsResponse) transactionsSOAPConnector.callWebService(request);
+        TransactionsResponse response = (TransactionsResponse) transactionsSOAPConnector.callWebService(request);
+        log.info("Received transaction data for customer number: {}", customerNumber);
+        return response;
     }
-
-
 }
